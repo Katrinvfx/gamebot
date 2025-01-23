2c5813dceae6f61f99be1ef91ff89838f92d0f39
katerinevfx <126059524+katerinevfx@users.noreply.github.com>
2025-01-23 04:03:18 +0300

    Initial commit

--- server.js
+++ server.js
@@ -4,16 +4,35 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Указываем, что index.html лежит в корне проекта
+// Разрешаем CORS
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Обслуживаем index.html
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Указываем папку со статическими файлами Unity WebGL
-app.use("/Build", express.static(path.join(__dirname, "Build")));
+// Обслуживаем статические файлы из папки Build с правильными заголовками
+app.use("/Build", express.static(path.join(__dirname, "Build"), {
+    setHeaders: (res, filePath) => {
+        if (filePath.endsWith(".wasm")) {
+            res.setHeader("Content-Type", "application/wasm");
+        }
+        if (filePath.endsWith(".js")) {
+            res.setHeader("Content-Type", "application/javascript");
+        }
+        if (filePath.endsWith(".data")) {
+            res.setHeader("Content-Type", "application/octet-stream");
+        }
+    }
+}));
 
 // Запуск сервера
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
